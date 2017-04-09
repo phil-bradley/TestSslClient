6474177b366e46a7520a41c9c7f0d6bbdf9d8ef2
phil-bradley <philb@mobius.ie>
2017-04-09 20:23:04 +0100

    SSL Client

---
+++ src/main/java/ie/philb/testclient/Client.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ie.philb.testclient;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+/**
+ *
+ * @author philb
+ */
+public class Client {
+
+    public static void main(String[] args) throws Exception {
+
+        // System.setProperty("javax.net.debug", "all");
+        TrustManager[] trustAllCerts = new TrustManager[]{
+            new X509TrustManager() {
+                @Override
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[0];
+                }
+
+                @Override
+                public void checkClientTrusted(
+                        java.security.cert.X509Certificate[] certs, String authType) {
+                }
+
+                @Override
+                public void checkServerTrusted(
+                        java.security.cert.X509Certificate[] certs, String authType) {
+                }
+            }
+        };
+
+//        String keyStoreFileName = "C:\\Users\\philb\\Documents\\NetBeansProjects\\TestClient\\testcert_pub.jks";
+//        char[] keyStorePassword = "password".toCharArray();
+//
+//        KeyStore ks = KeyStore.getInstance("JKS");
+//        ks.load(new FileInputStream(keyStoreFileName), keyStorePassword);
+//        
+//        KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+//        kmf.init(ks, keyStorePassword);
+        SSLContext sslcontext = SSLContext.getInstance("TLS");
+//        sslcontext.init(kmf.getKeyManagers(), trustAllCerts, null);
+        sslcontext.init(null, trustAllCerts, null);
+
+        SSLSocketFactory ssf = sslcontext.getSocketFactory();
+        SSLSocket s = (SSLSocket) ssf.createSocket("192.168.0.10", 1234);
+        s.startHandshake();
+
+        /**
+         * *********************************
+         */
+//        SSLSocketFactory ssf = (SSLSocketFactory) SSLSocketFactory.getDefault();
+//        Socket s = ssf.createSocket("127.0.0.1", 1234);
+        //Socket s = new Socket("localhost", 1234);
+        InputStream is = s.getInputStream();
+        OutputStream os = s.getOutputStream();
+        os.write("hello".getBytes());
+
+        int i;
+        StringBuilder sb = new StringBuilder();
+
+        while ((i = is.read()) != -1) {
+            char c = (char) i;
+            sb.append(c);
+        }
+
+        close(is);
+        close(os);
+        close(s);
+
+        System.out.println("Got -->" + sb + "<--");
+    }
+
+    private static void close(Socket s) {
+        if (s == null) {
+            return;
+        }
+        try {
+            s.close();
+        } catch (Exception ex) {
+        }
+    }
+
+    private static  void close(OutputStream os) {
+        if (os == null) {
+            return;
+        }
+        try {
+            os.close();
+        } catch (Exception ex) {
+        }
+    }
+
+    private static void close(InputStream is) {
+        if (is == null) {
+            return;
+        }
+        try {
+            is.close();
+        } catch (Exception ex) {
+        }
+    }
+}
